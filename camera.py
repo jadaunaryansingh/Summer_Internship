63c60f405d1e5afc5e2fd9219dfb4a449c7470ea
Aryan Singh Jadaun <coderaryanjadaun19@outlook.com>
2025-06-23 10:48:14 +0530

    Delete camera.py

--- camera.py
+++
@@ -1,9 +0,0 @@
-import cv2
-click=cv2.VideoCapture(0)
-while True:
-    status,photo=click.read()
-    cv2.imshow("Camera",photo)
-    if cv2.waitKey(10)==13:
-        cv2.destroyAllWindows()
-       click.release()
-       break
